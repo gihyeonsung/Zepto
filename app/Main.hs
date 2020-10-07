41e0c4d96aa5e848438b4339e1beb8bde8bd187d
Gihyeon Sung <gihyeon@gihyeon.xyz>
2020-10-07 23:34:23 +0900

    expr ast

--- app/Main.hs
+++ app/Main.hs
@@ -4,5 +4,5 @@ import Ast
 
 main :: IO ()
 main =
-  let ast = Expr $ Add 10 20 in
-  print ast
+  let ast = Expr $ Add (Lit $ Num 10) (Lit $ Num 20)
+   in print ast
--- src/Ast.hs
+++ src/Ast.hs
@@ -1,5 +1,24 @@
 module Ast where
 
-data Expr = Add Integer Integer deriving (Show)
+data Stmt
+  = Expr Expr
+  deriving (Show)
 
-data Stmt = Expr Expr deriving (Show)
+data Expr
+  = Add Expr Expr
+  | Sub Expr Expr
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Mod Expr Expr
+  | Assign Var Expr
+  | Lit Lit
+  | Var Var
+  deriving (Show)
+
+data Lit
+  = Num Integer
+  | Bool Bool
+  | String String
+  deriving (Show)
+
+newtype Var = V String deriving (Show)
